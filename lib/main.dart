b9ebf4ddff92df03c912082da0a1da4a285d08ad
ThornThavann <thavann.thorn@institute.pse.ngo>
2025-03-19 09:47:00 +0700

    ave

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
+// import 'package:bin_band_group/sceens/home_sceen.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//       ),
+//       home: const HomeScreen(),
+//     );
+//   }
+// }
+
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class LineChartWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -86,7 +104,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
@@ -121,3 +138,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
